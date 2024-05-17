559c23d1686b0e94cff22a0f6d76d091abde7bfa
thaddywu <thaddywu@gmail.com>
2024-05-17 20:52:03 +0000

    script dfa-equiv checks

--- src/eval.py
+++ src/eval.py
@@ -1,4 +1,25 @@
-from greenery import parse
-#parse("^[^e]*$").difference(
-for s in parse("~(.*e.*)")).strings():
-    print(s)
+import subprocess, json
+from tqdm import tqdm
+
+def dfa_equiv(gold, predicted):
+    # This function uses the regex_dfa_equals.jar to check the equivalence of two given regexes.
+    try:
+        out = subprocess.check_output(['java', '-jar', 
+                                       'deep-regex/deep-regex-model/regex_dfa_equals.jar', 
+                                       '{}'.format(gold), '{}'.format(predicted)])
+    except Exception as e:
+        return False
+    
+    return '\n1' in str(out)
+    # regex_dfa_equals.jar would return "[regex1]\n[regex2]\n[equivalent?1:0]"
+
+with open("outputs/KB13/plain.txt", "r") as f:
+    instances = json.load(f)
+    count_dfa_equiv = 0
+    for instance in tqdm(instances):
+        ground_truth = instance["ground_truth"]
+        chatgpt_output = instance["chatgpt_output"]
+        if dfa_equiv(ground_truth, chatgpt_output):
+            count_dfa_equiv += 1
+
+print(count_dfa_equiv)
--- src/query.py
+++ src/query.py
@@ -1,23 +1,38 @@
 from chatgpt import chatgpt
 from tqdm import tqdm
+import json
 
 def test_chatgpt():
     print(chatgpt("what's the capital of japan?"))
 
 def plain_prompt(nl):
     return f"""[Task]
-Please work as a expert regex generator. I'll give you a natural language description which corresponds to some regex.
+Please work as an expert regex translator. I'll give you a natural language description which corresponds to some regex.
 Please answer with a single line of regex consistent to the description and tries to keep it succinct and simple.
 [Input: natural language description]
 {nl}
 """
 
 def generate_regex_for_KB13():
-    with open(f"datasets/KB13/src.txt", "r") as fin:
-        with open(f"outputs/KB13/plain.txt", "w") as fout:
-            for l in tqdm(fin.readlines()):
-                if not l.strip(): continue
-                response = chatgpt(plain_prompt(l.strip())).strip()
-                print(response, file=fout)
+    responses = []
+    with open(f"datasets/KB13/src.txt", "r") as fnl:
+        nls = [l.strip() for l in fnl.readlines() if l.strip()]
+    with open(f"datasets/KB13/targ.txt", "r") as fgt:
+        gts = [l.strip() for l in fgt.readlines() if l.strip()]
+
+    for nl, gt in tqdm(zip(nls, gts)):
+        prompt = plain_prompt(nl)
+        responses += [{
+            "description": nl,
+            "prompt": prompt,
+            "ground_truth": gt,
+            "chatgpt_output": chatgpt(prompt).strip()
+        }]
+        print(nl)
+        print(responses[-1]["chatgpt_output"])
+    
+    with open(f"outputs/KB13/plain.txt", "w") as fout:
+        json.dump(responses, fout)
+        
 
 generate_regex_for_KB13()
